f87c3646d0812d2818377feebf065ae51f6c42d7
digitradiart <digitradiart@gmail.com>
2021-01-23 17:52:51 +0700

    learn

---
+++ progate/php/01-echo.php
@@ -0,0 +1,10 @@
+<?php
+    // echo 'Hello, world!'
+    echo 'Hello, world!';
+
+    // echo 7 * 2
+    echo 7 * 2;
+
+    // echo 8 % 3
+    echo 8 % 3;
+?>
---
+++ progate/python/28-rock-scissors-paper-modules.py
@@ -0,0 +1,6 @@
+# The code in our script.py file has become very long. As your code gets longer, it becomes harder to read and comprehend, and could lead you to unwanted bugs. To prevent this, let's learn how to divide code into separate files.
+# We can move parts of our code to a separate file, and import it as a module. A module is a file that can be imported into another file.
+# By importing a module, you can use the functions in that module.
+# We'll be moving some of the functions in script.py to utils.py.
+# You can call a function of a module by putting the name of the module before the function name, like module_name.function_name().
+
