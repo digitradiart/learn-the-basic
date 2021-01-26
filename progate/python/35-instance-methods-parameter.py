421e2fd5e151d6d7a7a9a3cfad4601f75e74f775
digitradiart <digitradiart@gmail.com>
2021-01-27 04:02:48 +0700

    progate

---
+++ progate/python/35-instance-methods-parameter.py
@@ -0,0 +1,5 @@
+# Let's review what we've learned so far about classes and instances.
+# A class is like a blueprint and an instance is an object that we create using that blueprint. The properties of an instance are called instance variables and functions are called instance methods. 
+
+# Method for the Total Price
+# Using what we've learned so far, let's make a method that calculates the total price. When passing arguments to the instance method, don't forget that self is automatically passed. Any arguments you input will go to the definition's second parameter and on.
