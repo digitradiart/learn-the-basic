efeeebc0e9578ace4886103b929809e141b258f4
digitradiart <digitradiart@gmail.com>
2021-03-31 04:42:52 +0700

    string

---
+++ PCAP-CISCO/module2/2619_lab_simple_input_output.py
@@ -0,0 +1,34 @@
+# LAB
+
+# Estimated time
+
+# 5-10 minutes
+# Level of difficulty
+
+# Easy
+# Objectives
+
+#     becoming familiar with the inputting and outputting of data in Python;
+#     evaluating simple expressions.
+
+# Scenario
+
+# Your task is to complete the code in order to evaluate the results of four basic arithmetic operations.
+
+# The results have to be printed to the console.
+
+# You may not be able to protect the code from a user who wants to divide by zero. That's okay, don't worry about it for now.
+
+# Test your code - does it produce the results you expect?
+
+# We won't show you any test data - that would be too simple.
+
+float1 = float(input("float: "))
+float2 = float(input("float: "))
+
+print(float1 + float2)
+print(float1 - float2)
+print(float1 * float2)
+print(float1 / float2)
+
+print("\nThat's all, folks!")
