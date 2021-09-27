fb32e3650be99cb494a6c910e76dcf632eb9617d
digitradiart <digitradiart@gmail.com>
2021-09-27 18:48:58 +0700

    27092021

---
+++ TLX-TOKI/main01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+   printf("Halo, dunia!");
+   return 0;
+}
---
+++ TLX-TOKI/main02.c
@@ -0,0 +1,11 @@
+// 03. Ekspresi dan Masukan/Keluaran
+
+#include <stdio.h>
+int main() {
+    int A, B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    printf("%d\n", A + B);
+
+    return 0;
+}
