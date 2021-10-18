a8861b172c511333c20e8f8a94c5a50a2dbe7b93
digitradiart <digitradiart@gmail.com>
2021-10-19 05:19:28 +0700

    basic javascript freecodecamp

---
+++ FREECODECAMP/freecodecamp_Javascript/comment-your-javascript-code.js
@@ -0,0 +1,4 @@
+// NOTE: As you write code, you should regularly add comments to clarify the function of parts of your code. Good commenting can help communicate the intent of your code—both for others and for your future self.
+
+/* NOTE: As you write code, you should regularly add comments to clarify the function of parts of your code.
+Good commenting can help communicate the intent of your code—both for others and for your future self. */
---
+++ FREECODECAMP/freecodecamp_Javascript/declare-string-variables.js
@@ -0,0 +1,2 @@
+var myFirstName = "NOT";
+var myLastName = "A NUMBER";
