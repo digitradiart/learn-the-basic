fc3261be00f2b08f3b55364d951d69c890f8f121
digitradiart <digitradiart@gmail.com>
2021-11-24 21:05:51 +0700

    ES6

---
+++ FREECODECAMP/02 JavaScript Algorithms and Data Structures/02 ES6/use-destructuring-assignment-to-extract-values-from-objects.js
@@ -0,0 +1,14 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+// const today = HIGH_TEMPERATURES.today;
+// const tomorrow = HIGH_TEMPERATURES.tomorrow;
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
