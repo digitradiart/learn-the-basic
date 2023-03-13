3700ffe98c3788cc78350f397483600d288c1d5b
digitradiart <digitradiart@gmail.com>
2023-03-13 08:49:49 +0700

    basic js dari PZN

---
+++ JS/c5-typeDataString.js
@@ -0,0 +1,16 @@
+document.writeln("</br>");
+document.writeln("TIPE DATA STRING");
+document.writeln("</br>");
+document.writeln("My name is Ayu");
+document.writeln("</br>");
+
+document.writeln("\nESCAPE SEQUENCE:");
+document.writeln("</br>");
+document.writeln("<textarea cols='50' rows='10'>");
+document.writeln("\nMy name \nis Ayu\n");
+document.writeln("\tMy name \tis Ayu");
+document.writeln('My name is "Ayu"');
+document.writeln("My name is 'Ayu'");
+document.writeln("\t\\My name is \\Ayu");
+
+document.writeln("</textarea>");
